8a2f076f89b3225e6ecc305d2c0376d0f7a2c68d
Se-mok, Choi <csm950907@naver.com>
2016-04-01 12:31:25 +0900

    Duplicated code change.

--- src/main/java/team09/week4/Gold.java
+++ src/main/java/team09/week4/Gold.java
@@ -8,32 +8,8 @@ public class Gold extends Rank{
 		super();
 		this._default = 1000;
 		this._exceedRate = 0.45f;
+		this._defaultFee = 49.95f;
 	}
 	
 	//public methods
-	@Override
-	public float Calc(float used){
-		float fees = 0;
-		fees = 49.95f + (this._lines - 1) * 14.95f + this.exceedFee(used);
-		
-		return fees;
-	}
-	
-	public void setLine(int line){
-		this._lines = line;
-	}
-	
-	public float exceedFee(float used){
-		float eFee = 0.f;
-		float exceedUse = this._default - used;
-		
-		if(exceedUse > 0)
-			return 0;
-		else{
-			exceedUse *= -1.f;
-			eFee = exceedUse * this._exceedRate;
-		}
-		
-		return eFee;
-	}
 }
