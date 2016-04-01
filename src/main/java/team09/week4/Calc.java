55a06a29725a64aa5f0763b41a97b81ff4f3a534
Se-mok, Choi <csm950907@naver.com>
2016-04-01 22:51:27 +0900

    change class Calc / User.

--- src/main/java/team09/week4/Calc.java
+++ src/main/java/team09/week4/Calc.java
@@ -1,6 +1,17 @@
 package team09.week4;
 
 public class Calc {
-	private int a;
-	private int b;
+	//private variables
+	private User _user;
+	
+	//constructor
+	public Calc(User user){
+		this._user = user;
+	}
+	
+	//public methods
+	float calcCharge(){
+		this._user.getRank().setLine(this._user.getLine());
+		return this._user.getRank().calc(this._user.getUsedRate());
+	}
 }
