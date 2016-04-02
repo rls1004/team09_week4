81c433a181dbb689a288dd91a00758285dfd641a
woojin <woojin@cse27>
2016-04-03 00:05:14 +0900

    app, output수정

--- src/main/java/team09/week4/App.java
+++ src/main/java/team09/week4/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Output output = new Output();
+        output.OutputBill();
     }
 }
--- src/main/java/team09/week4/Output.java
+++ src/main/java/team09/week4/Output.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 
 public class Output {
 	//private variables
-	
+	private User user;
 	
 	//constructor
 	public Output(){
-		
+		this.user = new User((float) 300,3,"Gold");
 	}
 	
 	//public methods
+	public void OutputBill()
+	{
+		System.out.println("사용한 데이터: " + user.getUsedRate());
+		System.out.println("사용 라인: " + user.getLine());
+		System.out.println("등급: " + user.getRank());
+	}
 }
